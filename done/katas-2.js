ffdf356b53f3eb5f4b1d9bc6d2ddca35062ba499
Veniaminwhs <veniamin208879@student-schoolsofwestfield.org>
2025-10-24 15:26:58 +0000

    cleanup

--- done/katas-2.js
+++ done/katas-2.js
@@ -48,3 +48,81 @@ function nextScoop(scoop){
     let flavor = prompt("Flavor for scoop number " + scoop);
     return flavor+" ";
 }
+
+
+/* RANDOM STUDENT KATA:  */
+
+// Who will be gatekeeper and keymaster today?
+
+/* Global Variables */
+// Create an array called students, start empty. 
+// Create an array called picked, start empty. 
+var students = [];
+var picked = [];
+/* Function Main()
+ * Set variable called count to asking how many students there are.
+ * Fill global array students by passing count as an argument to addStudents
+ * Set gateKeeper = randStudent(count), the name of a random student in the list
+ * Set keymaster = randNewStudent(), name of a different random student
+ * Display gatekeeper name and keymaster name for this kata
+ * @param = none
+ * @return = none
+ */
+function main(){
+    let count = prompt("How many students are there?");
+    addStudents(count);
+    let gateKeeper = randStudent();
+    let keyMaster = randNewStudent();
+    alert ("The gateKeeper = " + gateKeeper + " and the keymaster = " + keyMaster);
+}
+
+/* Function addStudents(count) 
+ * Use a for loop to add student names to the students array via user input.
+ * The loop should run count number of times. 
+ * @param = count {integer}
+ * @return = none
+ */
+function addStudents(count){
+     for (let student = 1; student <= count; student++){
+        students.push(prompt("next student? "));
+    }
+}
+
+
+/* Function randStudent()
+ * Set student = random number, 0 to length of students array.
+ * Set name = element in students array at that random index.
+ * Add name to picked array. 
+ * Return the name of the random student
+ * @param = count {integer}
+ * @return = name {string}
+ */
+function randStudent(){
+    let student = Math.floor(Math.random()*students.length);
+    let sName = students[student];
+    picked.push(sName);
+    return sName;
+}
+/* Function randNewStudent() (start with copy of randStudent)
+ * Set name = "", student = 0 and pickedYet = 0. 
+ * Loop while pickedYet is not equal to -1
+ * Set student = random number, 0 to length of students array.
+ * Set name = element in students array at that random index.
+ * Set pickedYet = the index of that name the picked array. 
+ * End the while loop.
+ * Add name to picked array.
+ * Return the name of the new random student
+ * @param = none
+ * @return = name {string}
+ */
+function randNewStudent(){
+    let sName = "";
+    let student = 0;
+    let pickedYet = 0;
+    while (pickedYet != -1){
+        student = Math.floor(Math.random()*students.length);
+        sName = students[student];
+        pickedYet = picked.indexOf(sName);
+    }
+    return sName;
+}
--- script.js
+++ script.js
@@ -5,60 +5,44 @@
 /* Global Variables */
 // Create an array called students, start empty. 
 // Create an array called picked, start empty. 
-var students = [];
-var picked = [];
+[]
 /* Function Main()
- * Set variable called count to asking how many students there are.
+ * Set variable count to asking how many students there are.
  * Fill global array students by passing count as an argument to addStudents
- * Set gateKeeper = randStudent(count), the name of a random student in the list
+ * Set gateKeeper = randStudent(), the name of a random student in the list
  * Set keymaster = randNewStudent(), name of a different random student
  * Display gatekeeper name and keymaster name for this kata
  * @param = none
  * @return = none
- */
-function main(){
-    let count = prompt("How many students are there?");
-    addStudents(count);
-    let gateKeeper = randStudent(count);
-    let keyMaster = randNewStudent();
-    alert ("The gateKeeper = " + gateKeeper + " and the keymaster = " + keyMaster);
-}
+ * /
 
-/* Function addStudents(count) 
- * Use a for loop to add student names to the students array via user input.
+/* Function addStudents(count)
+ * Use a for loop to add student names to the global students array.
  * The loop should run count number of times. 
+ * The loop should ask the user for a name and push it to the students array.
+ * Return the array of student names 
  * @param = count {integer}
  * @return = none
  */
-function addStudents(count){
-    students.push("Ven");
-    students.push("Igor");
-    students.push("Magan");
-}
 
-/* Function randStudent(count)
+/* Function randStudent()
  * Set student = random number, 0 to length of students array.
  * Set name = element in students array at that random index.
  * Add name to picked array. 
  * Return the name of the random student
- * @param = count {integer}
+ * @param = none
  * @return = name {string}
  */
-function randStudent(count){
-    return students[1];
-}
+ 
 /* Function randNewStudent() (start with copy of randStudent)
- * Set name = "", student = 0 and pickedYet = 0. 
- * Loop while pickedYet is not equal to -1
+ * Set student = "" and picked = 0. 
+ * Loop while picked is not equal to -1
  * Set student = random number, 0 to length of students array.
  * Set name = element in students array at that random index.
- * Set pickedYet = the index of that name the picked array. 
+ * Set picked = the index of that name the picked array. 
  * End the while loop.
  * Add name to picked array.
  * Return the name of the new random student
  * @param = none
  * @return = name {string}
  */
-function randNewStudent(){
-    return students[2];
-}
